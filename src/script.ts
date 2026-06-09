129f3deb8fbd976693b654bed969132b13c8f257
Luis-Vinicius-Severo <luislucasinter14@hotmail.com>
2026-06-09 18:10:33 -0300

    feat: adiciona módulo normalizarTransacao e refatora o código

--- src/script.ts
+++ src/script.ts
@@ -1,34 +1,12 @@
 import fetchData from './fetchData.js';
-
-type TransacaoPagamento = 'Bolete' | 'Cartão de Crédito';
-type TransacaoStatus =
-  | 'Paga'
-  | 'Aguardando pagamento'
-  | 'Recusada pela operadora de cartão'
-  | 'Estornada';
-
-interface TransacaoAPI {
-  Nome: string;
-  ID: number;
-  Data: string;
-  Status: TransacaoStatus;
-  Email: string;
-  ['Valor (R$)']: string;
-  ['Forma de Pagamento']: TransacaoPagamento;
-  ['Cliente Novo']: number;
-}
+import normalizarTransacao from './normalizarTransacao.js';
 
 async function handleData() {
   const data = await fetchData<TransacaoAPI[]>(
     'https://api.origamid.dev/json/transacoes.json',
   );
 
-  if (data) {
-    data.forEach((item) => {
-      console.log(item);
-    });
-  }
-
-  console.log(data);
+  if (!data) return;
+  const transacao = data.map(normalizarTransacao);
 }
 handleData();
