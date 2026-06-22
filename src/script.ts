8740675cc94e2bb819169fc6fccaf1297cb8a6a0
Luis-Vinicius-Severo <luislucasinter14@hotmail.com>
2026-06-22 17:27:31 -0300

    feat: adiciona EstatisticasTotal para calcular e exibir total em reais

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,4 @@
+import Estatisticas from './Estatisticas.js';
 import fetchData from './fetchData.js';
 import normalizarTransacao from './normalizarTransacao.js';
 
@@ -9,6 +10,19 @@ async function handleData() {
   if (!data) return;
   const transacoes = data.map(normalizarTransacao);
   preencherTabela(transacoes);
+  preencherEstatisticas(transacoes);
+}
+
+function preencherEstatisticas(transacoes: Transacao[]): void {
+  const data = new Estatisticas(transacoes);
+  const totalElement = document.querySelector<HTMLElement>('#total span');
+  if (totalElement) {
+    totalElement.innerText = data.total.toLocaleString('pt-br', {
+      style: 'currency',
+      currency: 'BRL',
+    });
+  }
+  console.log(data.total);
 }
 
 function preencherTabela(transacoes: Transacao[]): void {
